b98a35c1389a27249e0fd9f68772484dbea4e1db
thientoan0101 <66426299+thientoan0101@users.noreply.github.com>
2020-11-10 11:57:39 +0700

    Update Shell_Sort.cpp

--- Sort/Shell_Sort.cpp
+++ Sort/Shell_Sort.cpp
@@ -5,7 +5,7 @@
 //
 //
 // Steps :
-//    - S1 : Initialize shell size h0, h1, …, hk - 1 such that hi > hi + 1 and hk - 1 = 1;
+//    - S1 : Initialize shell size h0, h1, ï¿½, hk - 1 such that hi > hi + 1 and hk - 1 = 1;
 //    - S2: i = 0;
 //    - S3: Divide the original array into sub arrays with h[i] spacing.Sort each subarray using the insertion sort.
 //    - S4 : i = i + 1; If i < k, go to S2.
@@ -21,18 +21,21 @@ using namespace std;
 
 void shellSort(int a[], int n)
 {
-    for (int h = n / 2; h > 0; h = h / 2)
+    int h[10] = {100,75, 50, 25, 10, 5, 4, 3, 2, 1};
+    
+    for (int step = 0; step<10;step++)
     {
-        for (int i = h; i < n; i = i++)
+        int len = h[step];
+        for (int i = len; i < n; i = i++)
         {
             int x = a[i];
-            int pos = i - h;
+            int pos = i - len;
             while (pos >= 0 && a[pos] > x)
             {
-                a[pos + h] = a[pos];
-                pos = pos - h;
+                a[pos + len] = a[pos];
+                pos = pos - len;
             }
-            a[pos + h] = x;
+            a[pos + len] = x;
         }
     }
 }
